571c34a14009ff327fa931144f419e281ce6ec2c
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-26 00:21:13 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// let content = document.querySelector('.element');
+// content.addEventListener('onmouseover' , function(e){
+// const str = e;
+// console.log(str)
+// alert("IT is working")
+// })
+// function bigImg(x) {
+
+//     x.style.left = x.classList.add("active");
+//     x.style.right = x.classList.add("inactive");
+
+//   }
+//   function normalImg(x) {
+//     x.style.height = "100px";
+//     x.style.width = "100px";
+//   }
+//   window.addEventListener("mousemove",function(e){
+//     document.getElementById("cursor").style.left = e.pageX;
+//     document.getElementById("cursor").style.top = e.pageY;
+// })
+$('.card').tilt({
+  scale: 1.2
+})
